1a50c414e5e0081799354961adc7c4a4ea04c107
AveryJI <97696579+AveryJI@users.noreply.github.com>
2023-01-16 23:36:21 -0800

    should_always_pass_3=1+1->3=1+2

--- app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActvityTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActvityTest.java
@@ -1,2 +1,35 @@
-package edu.ucsd.cse110.lab2;public class BetterMainActvityTest {
+package edu.ucsd.cse110.lab2;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.widget.Button;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class BetterMainActvityTest {
+    @Test
+    public void test_one_plus_one_equals_two(){
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+        scenario.moveToState(Lifecycle.State.RESUMED);
+
+        scenario.onActivity(activity -> {
+            onView(withId(R.id.btn_one)).perform(click());
+            onView(withId(R.id.btn_plus)).perform(click());
+            onView(withId(R.id.btn_one)).perform(click());
+            onView(withId(R.id.btn_equals)).perform(click());
+            onView(withId(R.id.display)).check(matches(withText("2")));
+        });
+    }
 }
